6177f3a32396950e095ade8c189034a1cacff82f
ctordin <tordin1067@gmail.com>
2026-05-28 16:07:47 -0300

    Update radar_crypto_okx_LONG.py

--- radar_crypto_okx_LONG.py
+++ radar_crypto_okx_LONG.py
@@ -36,6 +36,7 @@ PORTFOLIO = {
     'WLD/USDT': 'WLD-USD',
     'OKB/USDT': 'OKB-USD',
     'BCH/USDT': 'BCH-USD',
+    'TRON/USDT': 'TRON-USD',
     'TSM': 'TSM' # Alterado para o Radar entender que é Especial
 }
 
